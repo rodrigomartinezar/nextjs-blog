69f0eee21805825b5dc8278bd7eb4efabb9e12fc
rodrigomartinezar <rodrigomartinezaraya97@gmail.com>
2020-08-18 11:32:28 -0400

    chore: change levels from segments

--- pages/posts/spiderchart.tsx
+++ pages/posts/spiderchart.tsx
@@ -1,7 +1,7 @@
 import SpiderChart from '../../components/Graph/spiderchart'
 
 const SpiderChartPost = () => {
-  let levels = 3
+  let levels = 5
 
   return <SpiderChart number_of_levels={levels}/>
 }
