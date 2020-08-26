bf30d2bc7ebb40b2ec106a0ee4c9fe196681b844
rodrigomartinezar <rodrigomartinezaraya97@gmail.com>
2020-08-26 15:31:07 -0400

    chore(Modal): modal closes when clicking outside of it

--- components/Graph/modal.tsx
+++ components/Graph/modal.tsx
@@ -3,6 +3,7 @@ import { Modal } from 'antd'
 const ModalComponent = (segmentName) => {
   Modal.info({
     title: segmentName,
+    maskClosable: true,
     content: (
       <div>Hola {segmentName}</div>
     )
